5b6fcc74a0d7c62ffceb340fca92f00df22dd2b0
HoaiSon <tranhoaison800@gmail.com>
2024-11-10 23:58:08 +0700

    read-stand-aero

--- pdf_readers/invoice_stand_aero.py
+++ pdf_readers/invoice_stand_aero.py
@@ -18,13 +18,29 @@ from datetime import datetime
 def classifier_invoice_stand_aero(pages):
     try:
         page_data = {}
-        dict_total = {}
-        list_table = []
-        list_table_description = []
-        extracting = False
-        charges = []
-        total_amount = None
+        list_table_description = {}
+        list_table_rotable_special_process = []
+        list_table_new_parts = []
+        list_table_new_lcf_parts = []
+        list_serviceable_parts = []
+        list_serviceable_lcf_parts = []
+        list_specially_priced_serviceable_parts = []
+        list_table_components_repair = []
+        replacement_parts = False
+        rotable_special_process = False
+        first_replacement_parts = False
+        extracted_replace_parts = False
+        specially_priced_serviceable_parts = False
+        new_lcf_parts = False
+        new_parts = False
+        serviceable_parts = False
+        serviceable_lcf_parts = False
+        components_repair = False
         key = keyword(stand_aero)
+        regex_new_parts = r'(\S+)\s+([A-Za-z0-9\s,#./()%]+)\s+(\d+)\s+(\d{1,3}(?:,\d{3})*(?:\.\d{2})?)\s+(\d{1,3}(?:,\d{3})*(?:\.\d{2})?)'
+        regex_rotable_special_process = r'(\S+)\s+([A-Za-z0-9\s,#/().]*)\s+(\d{1,3}(?:,\d{3})*(?:\.\d{2})?)\s+([\d-]+)'
+        regex_replacements_parts = r'^(?!Less:)([A-Za-z\s]+)\s+(\d{1,3}(?:,\d{3})*(?:\.\d{2})?)'
+        regex_replacements_parts_discount = r'Less:\s*([A-Za-z\s]+)\s+(\d{1,3}(?:\.\d+)?%)\s+\(\s*(\d{1,3}(?:,\d{3})*(?:\.\d{2})?)\)\s+(\d{1,3}(?:,\d{3})*(?:\.\d{2})?)'
         for p_idx, page in enumerate(pages):
             text = page.extract_text().split('\n')
             for i, line_row in enumerate(text):
@@ -34,7 +50,8 @@ def classifier_invoice_stand_aero(pages):
                     list_table_description[key.labor] = extract_value(line_row, key.labor)
                 if key.component_repair in line_row:
                     list_table_description[key.component_repair] = extract_value(line_row, key.component_repair)
-                if key.replacement_parts in line_row:
+                if key.replacement_parts in line_row and not first_replacement_parts:
+                    first_replacement_parts = True
                     list_table_description[key.replacement_parts] = extract_value(line_row, key.replacement_parts)
                 if key.rotable_or_special_processes in line_row:
                     list_table_description[key.rotable_or_special_processes] = extract_value(line_row, key.rotable_or_special_processes)
@@ -50,7 +67,7 @@ def classifier_invoice_stand_aero(pages):
                     list_table_description[key.sub_total] = extract_value(line_row, key.sub_total)
                 if key.shipping in line_row:
                     list_table_description[key.shipping] = extract_value(line_row, key.shipping)
-                if key.TOTAL in line_row:
+                if key.TOTAL in line_row and 'USD' in line_row:
                     list_table_description[key.TOTAL] = extract_value(line_row, key.TOTAL)
                 if key.invoice_number in line_row:
                     line_row = line_row.replace(' ', '')
@@ -58,17 +75,138 @@ def classifier_invoice_stand_aero(pages):
                     if match:
                         page_data[key.invoice_number] = match.group(1)
                 if key.date in line_row:
-                    print(line_row)
+                    print(list_table_description)
                     line_row = line_row.replace(' ', '')
                     match = re.search(r'Date:\s*(\d{4}-[A-Za-z]{3}-\d{2})', line_row)
                     if match:
                         date_value = match.group(1)
                         page_data[key.date] = date_value
-                        
+                if 'REPLACEMENT PARTS' in line_row and not extracted_replace_parts:
+                    replacement_parts = True
+                if replacement_parts:
+                    #print('in-rp')
+                    if 'TOTAL PARTS' in line_row:
+                        replacement_parts = False
+                        extracted_replace_parts = True
+                        page_data['total_parts'] = extract_value(line_row, '')
+                    else:
+                        match = re.search(regex_replacements_parts, line_row)
+                        if match:
+                            service_name = match.group(1)
+                            list_table_description[service_name] = extract_value(line_row, service_name)
+                            #print(i)
+                            match = re.search(regex_replacements_parts_discount, text[i+1])
+                            if match:
+                                list_table_description[service_name + 'Actual'] = to_float(match.group(4))
+                                list_table_description[match.group(1)] = to_float(match.group(2))
+                                list_table_description[service_name + 'Minus'] = to_float(match.group(3))    
+                if 'ROTABLE / SPECIAL PROCESS' in line_row:
+                    rotable_special_process = True
+                if rotable_special_process:
+                    if 'TOTAL ROTABLE / SPECIAL PROCESS' in line_row:
+                        rotable_special_process = False
+                        page_data['total_rotable_special_process'] = extract_value(line_row, '')
+                    else:
+                        #print(line_row)
+                        match = re.search(regex_rotable_special_process, line_row)
+                        if match:
+                            print(match.groups())
+                            model = Details()
+                            model.part_number = match.group(1)
+                            model.description = match.group(2)
+                            model.total = to_float(match.group(3))
+                            model.reference = match.group(4)
+                            list_table_rotable_special_process.append(model.to_dict())
+                if 'NEW PARTS' in line_row:
+                    new_parts = True
+                if new_parts:
+                    if 'TOTAL NEW PARTS' in line_row:
+                        new_parts = False
+                        page_data['total_new_parts'] = extract_value(line_row, '')
+                    else:
+                        match = re.search(regex_new_parts, line_row)
+                        if match:
+                            model = Details()
+                            model.part_number = match.group(1)
+                            model.description = match.group(2)
+                            model.quantity = to_float(match.group(3))
+                            model.list = to_float(match.group(4))
+                            model.total = to_float(match.group(5))
+                            list_table_new_parts.append(model.to_dict())
+                if 'NEW LCF PARTS' in line_row:
+                    new_lcf_parts = True
+                if new_lcf_parts:
+                    if 'TOTAL NEW LCF PARTS' in line_row:
+                        new_lcf_parts = False
+                        page_data['total_new_lcf_parts'] = extract_value(line_row, '')
+                    else:
+                        match = re.search(regex_new_parts, line_row)
+                        if match:
+                            model = Details()
+                            model.part_number = match.group(1)
+                            model.description = match.group(2)
+                            model.quantity = to_float(match.group(3))
+                            model.list = to_float(match.group(4))
+                            model.total = to_float(match.group(5))
+                            list_table_new_lcf_parts.append(model.to_dict())
+                if 'SERVICEABLE PARTS' in line_row:
+                    serviceable_parts = True
+                if serviceable_parts:
+                    if 'TOTAL SERVICEABLE PARTS' in line_row:
+                        serviceable_parts = False
+                        page_data['total_serviceable_parts'] = extract_value(line_row, '')
+                    else:
+                        match = re.search(regex_new_parts, line_row)
+                        if match:
+                            model = Details()
+                            model.part_number = match.group(1)
+                            model.description = match.group(2)
+                            model.quantity = to_float(match.group(3))
+                            model.list = to_float(match.group(4))
+                            model.total = to_float(match.group(5))
+                            list_serviceable_parts.append(model.to_dict())
+                if 'SERVICEABLE LCF PARTS' in line_row:
+                    serviceable_lcf_parts = True
+                if serviceable_lcf_parts:
+                    if 'TOTAL SERVICEABLE LCF PARTS' in line_row:
+                        serviceable_lcf_parts = False
+                        page_data['total_serviceable_lcf_parts'] = extract_value(line_row, '')
+                    else:
+                        match = re.search(regex_new_parts, line_row)
+                        if match:
+                            model = Details()
+                            model.part_number = match.group(1)
+                            model.description = match.group(2)
+                            model.quantity = to_float(match.group(3))
+                            model.list = to_float(match.group(4))
+                            model.total = to_float(match.group(5))
+                            list_serviceable_lcf_parts.append(model.to_dict())
+                if 'SPECIALLY PRICED SERVICEABLE PARTS' in line_row:
+                    specially_priced_serviceable_parts = True
+                if specially_priced_serviceable_parts:
+                    if 'TOTAL SPECIALLY PRICED SERVICEABLE PARTS' in line_row:
+                        specially_priced_serviceable_parts = False
+                        page_data['total_specially_priced_serviceable_parts'] = extract_value(line_row, '')
+                    else:
+                        match = re.search(regex_new_parts, line_row)
+                        if match:
+                            model = Details()
+                            model.part_number = match.group(1)
+                            model.description = match.group(2)
+                            model.quantity = to_float(match.group(3))
+                            model.list = to_float(match.group(4))
+                            model.total = to_float(match.group(5))
+                            list_specially_priced_serviceable_parts.append(model.to_dict())
                 #print(line_row)
                 logger.info("%s", line_row)
-        print(page_data)       
-        #print(invoice.to_string())           
+        page_data['description'] = list_table_description   
+        page_data['rotable_special_process'] = list_table_rotable_special_process
+        page_data['new_parts'] = list_table_new_parts
+        page_data['serviceable_parts'] = list_serviceable_parts
+        page_data['serviceable_lcf_parts'] = list_serviceable_lcf_parts
+        page_data['specially_priced_serviceable_parts'] = list_specially_priced_serviceable_parts
+        write_json_to_file(page_data)
+        #print(page_data)       
     except Exception as e:
         logger.error("Error invoice credit: %s", str(e))
         return None
