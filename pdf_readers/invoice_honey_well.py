1db11c00259f2099b3443b23f266c627802f44b3
Hieu <gacontapgayno1@gmail.com>
2024-11-10 08:28:21 +0700

    read-honey-well-invoice

--- pdf_readers/invoice_honey_well.py
+++ pdf_readers/invoice_honey_well.py
@@ -23,18 +23,95 @@ def classifier_honey_well_invoice(pages):
         invoice = Invoice()
         data_page_quote_totals = []
         is_quote_totals = False
+        list_table_new_or_replace_parts = []
+        list_table_repair_or_overhaul_parts = []
+        list_table_over_and_above_nor_parts = []
+        list_table_over_and_above_roo_parts = []
+        regex_nor = re.compile(r"(\d+)\s+(\S+)\s*(.*?)\s{2,}(.+?)\s{2,}(\S*)\s*(.*?)\s+(\d+\.\d+)\s+\$\s+([\d,]+\.\d+)\s+\$\s+([\d,]+\.\d+)\s+\$\s+([\d,]+\.\d+)")
+        regex_roo = re.compile(r"(\d{6})\s+(\S+)\s*(.*?)\s{2,}(.+?)\s{2,}(.+?)\s{2,}(.+?)\s{2,}(\d+\.\d{2})\s+\$\s+([\d,]+\.\d{2})\s+\$\s+([\d,]+\.\d{2})")
         key = keyword(honey_well)
+        new_or_replace_parts = False
+        repair_or_overhaul_parts = False
+        over_and_above_nor_parts = False
+        over_and_above_roo_parts = False
         for p_idx, page in enumerate(pages):
+            
             page_name = ''
             text = page.extract_text().split('\n')
             details = {}
             start_idx = None
             end_idx = None
             for i, line_row in enumerate(text):
+                #print(line_row)
                 if key.quote_totals in line_row:
                     if start_idx is None:
                         start_idx = i
                     end_idx = i 
+                try:
+                    if 'NEW/REPLACED PARTS' in line_row:
+                        new_or_replace_parts = True
+                    pass
+                except: pass
+                if new_or_replace_parts:
+                    
+                    if 'TOTAL NEW/REPLACED PARTS' in line_row:
+                        values = line_row.split('$')
+                        total_new_or_replace_parts = to_float(values[1].replace(' ', ''))
+                        page_data['total_new_or_replace_parts'] = total_new_or_replace_parts
+                        new_or_replace_parts = False
+                    else:
+                        match = regex_nor.search(line_row)
+                        if match:
+                            #print(match.groups())
+                            list_table_new_or_replace_parts.append(put_data_to_table_nor(match))
+                try:
+                    if 'REPAIR/OVERHAUL PARTS' in line_row:
+                        repair_or_overhaul_parts = True
+                    pass
+                except: pass
+                if repair_or_overhaul_parts:
+                    if 'TOTAL REPAIR/OVERHAUL PARTS' in line_row:
+                        values = line_row.split('$')
+                        total_repair_or_overhaul_parts = to_float(values[1].replace(' ', ''))
+                        page_data['total_repair_or_overhaul_parts'] = total_repair_or_overhaul_parts
+                        repair_or_overhaul_parts = False
+                    else:
+                        match = regex_roo.search(line_row)
+                        
+                        print(line_row)
+                        if match:
+                            print(match.groups())
+                            list_table_repair_or_overhaul_parts.append(put_data_to_table_roo(match))
+                try:
+                    if 'OVER & ABOVE NEW/REPLACED PARTS' in line_row:
+                        over_and_above_nor_parts = True
+                    pass   
+                except: pass
+                if over_and_above_nor_parts:
+                    if 'TOTAL NEW/REPLACED PARTS' in line_row:
+                        values = line_row.split('$')
+                        total_over_and_above_nor_parts = to_float(values[1].replace(' ', ''))
+                        page_data['total_over_and_above_nor_parts'] = total_over_and_above_nor_parts
+                        over_and_above_nor_parts = False
+                    else:
+                        match = regex_nor.search(line_row)
+                        if match:
+                            list_table_over_and_above_nor_parts.append(put_data_to_table_nor(match)) 
+                try:
+                    if 'OVER & ABOVE REPAIR/OVERHAUL PARTS' in line_row:
+                        over_and_above_roo_parts = True
+                    pass
+                except: pass
+                if over_and_above_roo_parts:
+                    if 'TOTAL REPAIR/OVERHAUL PARTS' in line_row:
+                        values = line_row.split('$')
+                        total_over_and_above_roo_parts = to_float(values[1].replace(' ', ''))
+                        page_data['total_over_and_above_roo_parts'] = total_over_and_above_roo_parts
+                        over_and_above_roo_parts = False
+                    else:
+                        match = regex_roo.search(line_row)
+                        if match:
+                            list_table_over_and_above_roo_parts.append(put_data_to_table_roo(match))
             if start_idx is not None and end_idx is not None and end_idx > start_idx:
                 result_lines = text[start_idx:end_idx+1]
                 for line in result_lines:
@@ -62,6 +139,7 @@ def classifier_honey_well_invoice(pages):
                 for table in tables:
                     for row in table:
                         processed_row = process_table_row(row)
+                        #print(processed_row)
                         logger.info("%s", processed_row)
         if len(data_page_quote_totals) > 0:
             page_name = 'quote_totals'
@@ -69,10 +147,48 @@ def classifier_honey_well_invoice(pages):
             page_data[page_name] = data_page_quote_totals 
         elif page_name and not is_quote_totals:
             page_data[page_name] = details
-
-
-        #write_json_to_file(page_data)
-        print(json.dumps(page_data, indent=4))     
+        page_data['new_or_replace_parts'] = list_table_new_or_replace_parts
+        page_data['repair_or_overhaul_parts'] = list_table_repair_or_overhaul_parts
+        page_data['over_and_above_nor_parts'] = list_table_over_and_above_nor_parts
+        page_data['over_and_above_roo_parts'] = list_table_over_and_above_roo_parts
+        write_json_to_file(page_data)
+        #print(json.dumps(page_data, indent=4))     
     except Exception as e:
         logger.error("Error invoice credit: %s", str(e))
+        return None
+@time_execution
+def put_data_to_table_roo(match):
+    try:
+        model = Details()
+        model.item = match.group(1)
+        model.part_number = match.group(2)
+        model.part_no_out = match.group(3)
+        model.description = match.group(4)
+        model.service_bulletin = match.group(5)
+        model.defect_description = match.group(6)
+        model.repair_or_overhaul_description = match.group(7)
+        model.quantity = to_int(match.group(8))
+        model.unit_price = to_float(match.group(9))
+        model.extend_price = to_float(match.group(10))
+        return model.to_dict()
+    except Exception as e:
+        print(f"Error: {e}")
+        return None   
+@time_execution
+def put_data_to_table_nor(match):
+    try:
+        model = Details()
+        model.item = match.group(1)
+        model.part_number = match.group(2)
+        model.part_no_out = match.group(3)
+        model.description = match.group(4)
+        model.service_bulletin = match.group(5)
+        model.defect_description = match.group(6)
+        model.quantity = to_int(match.group(7))
+        model.unit_price = to_float(match.group(8))
+        model.mhf = to_float(match.group(9))
+        model.extend_price = to_float(match.group(10))
+        return model.to_dict()
+    except Exception as e:
+        print(f"Error: {e}")
         return None
